2669710e3bf8ab625cf5ab81a2bc5a8a8ec30415
stevensmith-code <digitalexile99@gmail.com>
2022-12-18 18:04:37 -0600

    added changes to cost multiplier

--- src/Energy.jsx
+++ src/Energy.jsx
@@ -24,28 +24,6 @@ function Energy() {
   }
 
 
-  for (let i = 0; i < 10; i++) {
-  let generator = {
-    cost: Math.pow(10, i) + 20,
-    bought: 0,
-    multi: (i + 1) * 1.05
-  }   
-  shop.push(generator) 
-  }
-
-  for (let i = 0; i < 10; i++) {
-   let g = shop[i]
-   buttons.push(<div key={i} className={"border-2 cursor-pointer"}>
-    <p>Energy Increase {i + 1}</p>
-    <p>Cost: {format(g.cost)}</p>
-    <p>Bought: {format(g.bought)}</p>
-    <p>Multi: {format(g.multi)}</p>
-   </div>)
-    
-  }
-
-
-
   const energyGenerator = () =>{
 
     if(energy.accumulativeEnergy === energy.energyCap) return
@@ -56,6 +34,41 @@ function Energy() {
     accumulativeEnergy: prev.accumulativeEnergy += energy.energySpeed * energy.energyMultiplier,
     
     }))
+  }
+
+  const buyGenerator = i =>{
+    let g = shop[i - 1]
+    if(g.cost > energy.currentEnergy) return
+    energy.currentEnergy -= g.cost
+    g.bought += 1
+    g.multi *= 1.05
+    g.cost *= 1.5
+
+    setEnergy(prev => ({...prev,
+    
+    energyCap: Math.floor(prev.energyCap * g.multi)
+    }))
+  }
+
+
+  for (let i = 0; i < 10; i++) {
+  let generator = {
+    cost: Math.pow(10, i) * 10,
+    bought: 0,
+    multi: 1
+  }   
+  shop.push(generator) 
+  }
+
+  for (let i = 0; i < 10; i++) {
+   let g = shop[i]
+   buttons.push(<div key={i} className={"border-2 cursor-pointer"} onClick={()=> buyGenerator(i)}>
+    <p>Energy Increase {i + 1}</p>
+    <p>Cost: {format(g.cost)}</p>
+    <p>Bought: {format(g.bought)}</p>
+    <p>Multi: {format(g.multi)}</p>
+   </div>)
+    
   }
 
 
